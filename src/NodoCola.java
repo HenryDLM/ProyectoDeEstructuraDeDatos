a87c4aeded7478290ca6ca76678f41adcf8a657f
Henry Mora <55956517+BlitzHenry@users.noreply.github.com>
2019-11-10 15:20:16 -0600

    Add files via upload

---
+++ src/NodoCola.java
@@ -0,0 +1,37 @@
+public class NodoCola {
+   // Atributos 
+    private InfoPacientes elementoCola;
+    private NodoCola siguienteCola; 
+  // Redefinimos los atributos
+    public NodoCola() {
+         this.siguienteCola=null; 
+       
+        // utilizamos los el get y el set 
+       // Para modificar y leer 
+    }
+
+    public NodoCola getSiguienteCola() {
+        return siguienteCola;
+    }
+
+    public void setSiguienteCola(NodoCola siguienteCola) {
+        this.siguienteCola = siguienteCola;
+    }
+
+    public InfoPacientes getElementoCola() {
+        return elementoCola;
+    }
+
+    public void setElementoCola(InfoPacientes elementoCola) {
+        this.elementoCola = elementoCola;
+    }
+
+    @Override
+    public String toString() {
+        return "NodoCola{" + "elementoCola=" + elementoCola + ", siguienteCola=" + siguienteCola + '}';
+    }
+    
+    
+}
+
+    
